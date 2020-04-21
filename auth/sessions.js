4dbae72b97eeabe4bf8d5fcb5c1279ea437e98a3
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-21 12:42:26 -0400

    Separate sessions code from JWT

---
+++ auth/sessions.js
@@ -0,0 +1,59 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+
+const Users = require("../users/users-model.js");
+
+router.post("/register", (req, res) => {
+  let user = req.body; // username, password
+  // rounds are 2 to the N times
+  const rounds = process.env.HASH_ROUNDS || 8;
+
+  // hash the user.password
+  const hash = bcrypt.hashSync(user.password, rounds);
+
+  // update the creds to use the hash
+  user.password = hash;
+
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: error.message });
+    });
+});
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+  // Search for the user using the username
+  Users.findBy({ username })
+    .then(user => {
+      // console.log("user", user[0]);
+      // If we find the user, then also check that the passwords match
+      if(user && bcrypt.compareSync(password, user[0].password)) {
+        req.session.loggedIn = true;
+        res.status(200).json({ message: "Welcome!" });
+      } else {
+        res.status(401).json({ message: "You cannot pass!"})
+      }
+      
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: error.message });
+    });
+});
+
+router.get("/logout", (req, res) => {
+  if(req.session) {
+  req.session.destroy(error => {
+    error ? res.status(500).json({ errorMessage: "Unable to logout"}) : res.status(204).end();
+  });
+} else {
+  res.status(204).end();
+}
+});
+
+module.exports = router;
