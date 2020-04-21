d178cce945cd9a3d55944010d388f92744283b6d
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-21 13:51:11 -0400

    Update auth middleware to verify token

--- auth/authenticator.js
+++ auth/authenticator.js
@@ -1,3 +1,24 @@
+const jwt = require("jsonwebtoken");
+
 module.exports = (req, res, next) => {
-    next();
+    // Tokens are usually sent as the auth header
+    const token = req.headers.authorization;
+
+    const secret = process.env.JWT_SECRET || "blahblahblah";
+    
+    if(token) {
+
+    // Verify token
+    jwt.verify(token, secret, (error, decodedToken) => {
+        // If token is good, the error will be undefined
+        if(error) {
+            res.status(401).json({ message: "Cannot pass!" })
+        } else {
+            req.decodedToken = decodedToken;
+            next();
+        }
+});
+} else {
+    res.status(400).json({ message: "Please provide credentials" })
+}
 };
